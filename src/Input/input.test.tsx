9def680db3fdaa70bfdb3b5e0466d726d4eab774
swfoodt <1801263727@qq.com>
2023-02-17 10:08:17 +0800

    fix: input component

--- src/Input/input.test.tsx
+++ src/Input/input.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import Input, { type InputProps } from '.'
 import '@testing-library/jest-dom'
 
@@ -12,19 +12,19 @@ describe('test Input component', () => {
     const wrapper = render(<Input { ...defaultProps }/>)
     const testNode = wrapper.getByPlaceholderText('test-input') as HTMLInputElement
     expect(testNode).toBeInTheDocument()
-    expect(testNode).toHaveClass('viking-input-inner')
+    expect(testNode).toHaveClass('input-inner')
     fireEvent.change(testNode, { target: { value: '23' } })
     expect(defaultProps.onChange).toHaveBeenCalled()
     expect(testNode.value).toEqual('23')
   })
   it('should render the disabled Input on disabled property', () => {
-    const wrapper = render(<Input disable placeholder = "disabled"/>)
+    const wrapper = render(<Input disabled={true} placeholder = "disabled"/>)
     const testNode = wrapper.getByPlaceholderText('disabled') as HTMLInputElement
     expect(testNode.disabled).toBeTruthy()
   })
   it('should render different input sizes on size property', () => {
     const wrapper = render(<Input placeholder = "sizes" size = "lg"/>)
-    const testContainer = wrapper.container.querySelector('viking-input-inner')
+    const testContainer = screen.getByTestId('input-wrapper')
     expect(testContainer).toHaveClass('input-size-lg')
   })
   it('should render prepand and append element on prepand/append property', () => {
