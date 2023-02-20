7401f85a4b882630469ca41c787d5b43de6e3d1b
swfoodt <1801263727@qq.com>
2023-02-20 21:10:16 +0800

    fix: progress component、upload component
    fix: progress component、upload component

--- src/Upload/uploadList.tsx
+++ src/Upload/uploadList.tsx
@@ -1,45 +1,46 @@
 import React, { FC } from 'react'
 import { UploadFile } from '.'
-import Icon from '../Icon'
-import Progress from '../Progress'
-
+import { Icon } from 'triangle-ui'
+import { Progress } from 'triangle-ui'
 interface UploadListProps {
-  fileList: UploadFile[]
-  onRemove: (_file: UploadFile) => void
+  fileList: UploadFile[];
+  onRemove: (_file: UploadFile) => void;
 }
 
 export const UploadList: FC<UploadListProps> = (props) => {
   const {
     fileList,
-    onRemove
+    onRemove,
   } = props
+
   return (
-    <ul className = "viking-upload-list">
-      { fileList.map(item => {
+    <ul className="viking-upload-list">
+      {fileList.map(item => {
         return (
-        <li className = "viking-upload-list-item" key = { item.uid }>
-          <span className = { `file-name file-name-${item.status}` }>
-            <Icon icon = "file-alt" theme = "secondary"/>
-            { item.name }
-          </span>
-          <span className = "file-status">
-            { item.status === 'uploading' && <Icon icon = "spinner" spin theme = "primart"/>}
-            { item.status === 'success' && <Icon icon = "check-circle" theme = "success"/>}
-            { item.status === 'error' && <Icon icon = "times-circle" theme = "danger"/>}
-          </span>
-          <span className = 'file-actions'>
-            <Icon icon = "times" onClick = { () => { onRemove(item) } }/>
-          </span>
-          { item.status === 'uploading' &&
-            <Progress
-              percent = { item.percent ?? 0 }
-            />
-          }
-        </li>
+          <li className="viking-upload-list-item" key={item.uid}>
+            <span className={`file-name file-name-${item.status}`}>
+              <Icon icon="file-alt" theme="secondary" />
+              {item.name}
+            </span>
+            <span className="file-status">
+              {(item.status === 'uploading' || item.status === 'ready') && <Icon icon="spinner" spin theme="primary" />}
+              {item.status === 'success' && <Icon icon="check-circle" theme="success" />}
+              {item.status === 'error' && <Icon icon="times-circle" theme="danger" />}
+            </span>
+            <span className="file-actions">
+              <Icon icon="times" onClick={() => { onRemove(item)}}/>
+            </span>
+            {item.status === 'uploading' && 
+              <Progress 
+                percent={item.percent || 0}
+              />
+            }
+          </li>
         )
       })}
     </ul>
   )
+
 }
 
-export default UploadList
+export default UploadList;
